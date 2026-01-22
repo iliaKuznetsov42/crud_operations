40ba7330fc60165e7324a2b01d0f7f96f133f432
vboxuser <vboxuser@WIN10>
2026-01-22 12:37:17 +0300

    добавлен код

---
+++ crud_operations/View/Windows/AddProductWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using crud_operations.Model;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace crud_operations.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProductWindow.xaml
+    /// </summary>
+    public partial class AddProductWindow : Window
+    {
+        public AddProductWindow()
+        {
+            InitializeComponent();
+
+            CategoryCmb.ItemsSource = App.context.Category.ToList();
+            CategoryCmb.SelectedIndex = 0;
+            CategoryCmb.DisplayMemberPath = "Name";
+            CategoryCmb.SelectedValuePath = "id";
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(String.IsNullOrEmpty(NameTb.Text) &&
+                String.IsNullOrEmpty(PriceTb.Text) &&
+                String.IsNullOrEmpty(DescriptionTb.Text) &&
+                String.IsNullOrEmpty(CategoryCmb.Text) &&
+                String.IsNullOrEmpty(ReleaseDateDp.Text) &&
+                String.IsNullOrEmpty(PhotoTb.Text))
+            {
+                MessageBox.Show("Заполните все поля", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                Product product = new Product();
+                {
+                    Name = NameTb.Text,
+                    Price = Convert.ToDecimal(PriceTb.Text),
+                    Description = DescriptionTb.Text,
+                    Category = CategoryCmb.SelectedItem as Category,
+                    ReleaseDate = (DateTime)ReleaseDateDp.SelectedDate,
+                    IsAvaliable = Convert.ToBoolean(IsAvaliableCb.IsChecked)                    
+                };
+                App.context.Product.Add(product);
+                App.context.SaveChanges();
+
+                MessageBox.Show("Товар успешно длбавлен", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+            }
+        }
+
+        private void LoadFromPCBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                PhotoTb.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
--- crud_operations/View/Windows/MainWindow.xaml.cs
+++ crud_operations/View/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using crud_operations.Model;
+using crud_operations.View.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,7 +31,11 @@ namespace crud_operations
 
         private void AddProduct_Click(object sender, RoutedEventArgs e)
         {
-
+            AddProductWindow addProductWindow = new AddProductWindow();
+            if (addProductWindow.ShowDialog() == true)
+            {
+                ProductsLv.ItemsSource = App.context.Product.ToList();
+            }
         }
 
         private void EditPtoductBtn_Click(object sender, RoutedEventArgs e)
@@ -39,7 +45,23 @@ namespace crud_operations
 
         private void RemoveProductBtn_Click(object sender, RoutedEventArgs e)
         {
+            if(ProductsLv.SelectedItem != null) 
+            {
+                Product selectedProduct = ProductsLv.SelectedItem as Product;
 
+                MessageBoxResult result = MessageBox.Show("Вы действительно хотите удалить товар?", "Вопрос", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if(result == MessageBoxResult.Yes)
+                {
+                    App.context.Product.Remove(selectedProduct);
+
+                    MessageBox.Show("Товар удалён", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    App.context.SaveChanges();
+
+                    ProductsLv.ItemsSource = App.context.Product.ToList();
+                }
+            }
         }
     }
 }
